35e31274d11e3561421eb2fdc0680d648f9d97f2
rzy <ruziyi@qq.com>
2017-02-06 09:46:44 +0800

    修改redis链接

--- src/queue/RedisQueue.php
+++ src/queue/RedisQueue.php
@@ -5,27 +5,32 @@ use Redis;
 
 class RedisQueue implements QueueInterface
 {
+    private $connected = 0;
+    private $redis;
+
     public function getTask()
     {
-        $redis = $this->createRedis();
+        $redis = $this->getRedis();
         $task = $redis->lpop('task1-failed');
         if (!$task) {
             $task = $redis->blpop('task1', 10);
         }
         $task = $task ? $task[1] : null;
-        $redis->close();
         unset($redis);
         return $task;
     }
 
-    private function createRedis()
+    private function getRedis()
     {
-        $redis = new Redis();
-        $ret = $redis->connect('127.0.0.1');
-        if (!$ret) {
-            trigger_error('connect redis-server failed');
+        if (!$this->connected) {
+            $this->redis = new Redis();
+            $ret = $this->redis->pconnect('127.0.0.1');
+            if (!$ret) {
+                trigger_error('connect redis-server failed');
+            }
+            $this->connected = 1;
         }
-        return $redis;
+        return $this->redis;
     }
 
     public function putTask($task, $type="r")
@@ -33,13 +38,12 @@ class RedisQueue implements QueueInterface
         if (!is_string($task)) {
             $task = serialize($task);
         }
-        $redis = $this->createRedis();
+        $redis = $this->getRedis();
         if ($type == 'l') {
             $redis->lpush('task1', $task);
         } else {
             $redis->rpush('task1', $task);
         }
-        $redis->close();
     }
 
     public function putFailedTask($task)
@@ -47,8 +51,7 @@ class RedisQueue implements QueueInterface
         if (!is_string($task)) {
             $task = serialize($task);
         }
-        $redis = $this->createRedis();
+        $redis = $this->getRedis();
         $redis->rpush('task1-failed', $task);
-        $redis->close();
     }
 }
